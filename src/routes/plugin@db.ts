1a16edde751ffb9c9dbdde9f11801df8394f419d
LigeronAhill <provotorov@inbox.ru>
2026-01-13 18:02:02 +0300

    remove db plugin

--- src/routes/plugin@db.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler } from "@builder.io/qwik-city";
-import { initializeRepository } from "../../drizzle/db";
-
-export const onRequest: RequestHandler = ({ env }) => {
-    const dbURL = env.get("DATABASE_URL");
-    if (!dbURL)
-        throw new Error("DATABASE_URL environment variable must be set!");
-    initializeRepository(dbURL);
-};
